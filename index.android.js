4856592be3a8836ce85749156db0801136793e1b
user151509 <user151509@74-80-245-10.ic2net.net>
2019-09-30 11:05:42 -0700

    Changed Redux Structure

--- index.android.js
+++ index.android.js
@@ -1,12 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-const store = configureStore();
-
-const RNRedux = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
-);
-
-AppRegistry.registerComponent('myrealproject', () => RNRedux);
+AppRegistry.registerComponent('myrealproject', () => App);
--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,4 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from './src/store/configureStore';
 import { AppRegistry } from 'react-native';
 import App from './App';
-
-// const store = configureStore();
-
-// const RNRedux = () => (
-//     <Provider store={store}>
-//         <App />
-//     </Provider>
-// );
 
 AppRegistry.registerComponent('myrealproject', () => App);
